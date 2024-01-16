68999094a4f63ff8d30bddd637b6e4751e86b778
Mohamed <mohamed.laymouna@gmail.com>
2024-01-16 02:00:57 +0200

    add email verification and activate the user whenever he clicks on the link

--- models/user.py
+++ models/user.py
@@ -9,6 +9,7 @@ class UserModel(db.Model):
     phone = db.Column(db.String(12), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
+    is_verified = db.Column(db.Boolean, default=False)
 
     products = db.relationship("ProductModel", back_populates="owner", lazy="dynamic")
 
